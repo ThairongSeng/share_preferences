a4fc9ec514c6b2eda2c1f8e74578cb70702df7ad
ThairongSeng <seng.thairong@gmail.com>
2023-09-27 13:24:34 +0700

    add dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:share_reference/profile_screen.dart';
 import 'package:share_reference/splash_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'model_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,23 +16,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return ChangeNotifierProvider(
+        create: (_) => ModelTheme(),
+        child: Consumer<ModelTheme>(
+            builder: (context, ModelTheme themeNotifier, child) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: themeNotifier.isDark
+                ? ThemeData(
+                    brightness: Brightness.dark,
+                  )
+                : ThemeData(
+                    brightness: Brightness.light,
+                    primaryColor: Colors.green,
+                    primarySwatch: Colors.green),
+            debugShowCheckedModeBanner: false,
+            home: const MyHomePage()
+          );
+        }));
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -39,14 +48,14 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   bool newLaunch = false;
 
-  void _loadNewLaunch() async{
+  void _loadNewLaunch() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       newLaunch = (prefs.getBool("newLaunch")) ?? true;
     });
   }
 
-  void _loadCounter() async{
+  void _loadCounter() async {
     SharedPreferences pre = await SharedPreferences.getInstance();
     setState(() {
       _counter = pre.getInt('counter')!;
@@ -61,25 +70,18 @@ class _MyHomePageState extends State<MyHomePage> {
     _loadNewLaunch();
   }
 
-  void _incrementCounter() async{
-    SharedPreferences pre= await SharedPreferences.getInstance();
+  void _incrementCounter() async {
+    SharedPreferences pre = await SharedPreferences.getInstance();
     setState(() {
       _counter = (pre.getInt('counter') ?? 0) + 1;
       pre.setInt('counter', _counter);
-
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: newLaunch ? const SplashScreen() : const ProfileScreen(),
-
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:share_reference/model_theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -16,19 +18,37 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Profile Screen"),
-            ElevatedButton(
-                onPressed: () => clearSharePreference(),
-                child: const Text("Clear SharedPreference")
-            )
-          ],
-        )
-      ),
+    return  Consumer<ModelTheme>(
+      builder: (context, ModelTheme themeNotifier, child ){
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(themeNotifier.isDark ? "Dark Mode" : "Light Mode"),
+            actions: [
+              IconButton(
+                  icon: Icon(themeNotifier.isDark
+                      ? Icons.nightlight_round
+                      : Icons.wb_sunny),
+                  onPressed: () {
+                    themeNotifier.isDark
+                        ? themeNotifier.isDark = false
+                        : themeNotifier.isDark = true;
+                  })
+            ],
+          ),
+          body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Profile Screen"),
+                  ElevatedButton(
+                      onPressed: () => clearSharePreference(),
+                      child: const Text("Clear SharedPreference")
+                  )
+                ],
+              )
+          ),
+        );
+      },
     );
   }
 }
